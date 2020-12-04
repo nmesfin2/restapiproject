e6aaa12f765aa3e5774ef15dd19f4d030fefa3e1
Natnael Tezera <natnaeltezera@outlook.com>
2020-12-03 18:33:50 -0800

    OrganizationRestApi: initial commit, model implemented

---
+++ OrganizationRestApi/src/test/java/com/nat/organizatioinrestapi/OrganizatioinRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nat.organizatioinrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganizatioinRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
